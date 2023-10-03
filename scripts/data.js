4e7533ac80cd2b18dadcbe98bc9087637da6a94f
John Gonzalez <47705426+JGomez49@users.noreply.github.com>
2023-10-03 15:22:29 -0600

    First Commit.

---
+++ scripts/data.js
@@ -0,0 +1,248 @@
+var stages = {
+    stage1: {
+        data: [
+
+            {
+              "question": "Que es un survey?",
+              "options": [
+                "A: Magnetismo, Inclinacion y Altitud",
+                "B: Magnetismo, Inclinacion y Azimuth",
+                "C: MD, Inclinacion y Azimuth",
+                "D: TVD, Inclinacion y Azimuth",
+              ],
+              "ans": "C"
+            },
+
+            {
+              "question": "Cual de estas empresas es una operadora?",
+              "options": [
+                "A: Phoenix",
+                "B: CNRL",
+                "C: Mostar",
+                "D: Optimax",
+             ],
+             "ans": "B"
+            },
+
+            {
+              "question": "Que signinfica MD?",
+              "options": [
+                "A: Mean Depth",
+                "B: MWD",
+                "C: Measured Depth",
+                "D: Measured Drilling",
+              ],
+              "ans": "C"
+            },
+
+            {
+              "question": "Que es Inclinacion?",
+              "options": [
+                "A: Es la desviacion de la horizontal y la vertical",
+                "B: El el angulo formado entre la horizontal y la posicion de medir",
+                "C: Es el angulo formado entre la vertical y la posicion actual",
+                "D: Es el recorrido de un pozo hasta la posicion actual",
+              ],
+              "ans": "C"
+            },
+
+            {
+              "question": "Que es Azimuth?",
+              "options": [
+                "A: Es el angulo formado entre la direccion del pozo y el norte de referencia",
+                "B: Es el recorrido de un pozo hasta la posicion actual",
+                "C: Es el angulo formado desde la direccion del pozo actual y el survey ",
+                "D: Es el angulo formado entre la direccion del pozo y el norte verdadero",
+              ],
+              "ans": "A"
+            },
+
+            {
+              "question": "Que es Excel?",
+              "options": [
+                "A: Una extraordinaria base de datos",
+                "B: Un excelente lenguaje de programacion",
+                "C: Un buen sistema grafico de datos",
+                "D: Una poderosa hoja de calculo",
+              ],
+              "ans": "D"
+            },
+
+            {
+              "question": "Que es hace Metacortex?",
+              "options": [
+                "A: Provee de servicios de well planning y perforacion a otras empresas del sector petrolero",
+                "B: Provee de servicios de well planning e ingeniria de perforacion a otras empresas del sector petrolero",
+                "C: Provee de servicios de well planning empresas operadoras",
+                "D: Provee de servicios perforacion a otras empresas del sector petrolero",
+              ],
+              "ans": "B"
+            }
+
+        ]
+    },
+    stage2: {
+      data: [
+
+          {
+            "question": "Que es un MWD?",
+            "options": [
+              "A: Una herramienta para tomar surveys",
+              "B: Un metodo de calculo de surveys",
+              "C: Una marca de herramientas",
+              "D: significa Movie While Drilling",
+            ],
+            "ans": "A"
+          },
+
+          {
+            "question": "Que es Compass?",
+            "options": [
+              "A: Una bases de datos",
+              "B: Un programa para diseñar trajectorias a perforar",
+              "C: Una hoja de calculo",
+              "D: Un programa para dibujar pozos",
+           ],
+           "ans": "B"
+          },
+
+          {
+            "question": "Que es PDF?",
+            "options": [
+              "A: Es una extension de archivo para procesadores de palabra",
+              "B: Es un procesador de palabra",
+              "C: Es un reporte",
+              "D: Es un formato de archivo para ser independiente del sistema operativo",
+            ],
+            "ans": "D"
+          },
+
+          {
+            "question": "Que es el petroleo?",
+            "options": [
+              "A: Es dinosario liquado",
+              "B: Es un reservorio de gasolina debajo de la tierra",
+              "C: Es una mezcla de compuestos orgánicos, principalmente hidrocarburos insolubles en agua",
+              "D: Es un liquido negro originado desde el centro de la tierra",
+            ],
+            "ans": "C"
+          },
+
+          {
+            "question": "Que es CNRL?",
+            "options": [
+              "A: Una operadora. Significa Canadian Natural Resources Limited", 
+              "B: Una empresa de servicios petroleros. Significa Canadian Natural Resources Limited",
+              "C: Una operadora. Significa Canadian Natural Resorts Limited",
+              "D: Una perforadora. Significa Canadian Natural Reports Limited",
+            ],
+            "ans": "A"
+          },
+
+          {
+            "question": "Que servidores son de Metacortex?",
+            "options": [
+              "A: MTX_Compass y Horton",
+              "B: PlanninVM28 y PlanningVM39",
+              "C: MTX_Bridge y TeamViewer",
+              "D: MTX_Compass y MTX_Bridge",
+            ],
+            "ans": "D"
+          },
+
+          {
+            "question": "Que programas se usan para conectarse a otros computadores en Metacortex?",
+            "options": [
+              "A: TeamViewer y Chrome Remote Desktop",
+              "B: Remote Desktop Connection y Chrome Remote Desktop",
+              "C: TeamViewer, Remote Desktop Connection y Chrome Remote Desktop",
+              "D: TeamViewer y Remote Desktop Connection",
+            ],
+            "ans": "C"
+          }
+
+      ]
+  },
+  stage3: {
+    data: [
+
+        {
+          "question": "Que es un sistema operativo?",
+          "options": [
+            "A: Un programa informatico que permite administrar los recursos del equipo",
+            "B: Un buscador de paginas web",
+            "C: Un motor de busqueda para archivos y programas en el computador",
+            "D: Una unidad de hardware que se usa para almacenar contenido y datos digitales en las computadoras",
+          ],
+          "ans": "A"
+        },
+
+        {
+          "question": "Que es internet?",
+          "options": [
+            "A: Es una computadora central que maneja el trafico de datos",
+            "B: Se trata de un sistema de redes interconectadas mediante distintos protocolos",
+            "C: Es un sistema operativo para diferentes computadoras",
+            "D: Es un protocolo de datos para intercambiar informacion",
+         ],
+         "ans": "B"
+        },
+
+        {
+          "question": "Cual de los siguientes es un sistema operativo?",
+          "options": [
+            "A: Excel",
+            "B: WhatsApp",
+            "C: Windows",
+            "D: Word",
+          ],
+          "ans": "C"
+        },
+
+        {
+          "question": "Se pueden hacer tablas directamente es Word?",
+          "options": [
+            "A: No",
+            "B: Si, pero solo si la pegamos desde Excel",
+            "C: No, a menos que la peguemos como foto",
+            "D: Si",
+          ],
+          "ans": "D"
+        },
+
+        {
+          "question": "Que es Conveyor?",
+          "options": [
+            "A: Es una sistema en la web para gestionar y monitorear los proyectos de Metacortex",
+            "B: Es una herramienta para generar planes",
+            "C: Es un sistema que sirve para reportar los planes",
+            "D: Es un artilujio para hacer las carpetas para los jobs de Metacortex",
+          ],
+          "ans": "A"
+        },
+
+        {
+          "question": "Que nortes tiene la tierra?",
+          "options": [
+            "A: True, Grid y Azimuth",
+            "B: Declination, Grid y Azimuth",
+            "C: Declination, Convergence y Magnetic",
+            "D: True, Grid y Magnetic",
+          ],
+          "ans": "D"
+        },
+
+        {
+          "question": "Cual de los siguientes no es un tipo de pozo?",
+          "options": [
+            "A: Horizontal",
+            "B: Multivertical",
+            "C: J",
+            "D: S",
+          ],
+          "ans": "B"
+        }
+
+    ]
+  }
+}
---
+++ scripts/main.js
@@ -0,0 +1,333 @@
+
+
+class Game {
+    
+  constructor() {
+    this.stageLength = 1
+    this.answeredQuestion = []
+    this.gameLength = $(".stages button").length
+    this.pLen = this.gameLength
+    this.amount = $(".stages button").eq(this.pLen-1).html()
+    this.enableAskAudienceLifeline = true
+    this.enableCallFriendLifeline = true
+    this.enableFiftyFiftyLifeline = true
+    this.play = true
+    this.selected = ''
+    this.answer = ''
+  }
+
+  getRandomNumber() {
+    let random = Math.floor(Math.random()*7)
+    try{
+      while(this.answeredQuestion.toString().match(random)){
+        random = Math.floor(Math.random()*7);
+      }
+      this.answeredQuestion.push(random);
+      return random;
+    }
+    catch(e){}
+  }
+
+  changeQuestionStageData(data) {
+    let div
+    $(".options").empty()
+      $('.question').html(data["question"])
+      data["options"].map((option, i) => {
+        div = `<div class="${option.slice(0,1)} opt" key="${i}">${option}</div>`
+        $(".options").append(div)
+      })
+
+      $(".opt").click((e) => {
+        this.selected = e.target.textContent.slice(0,1)
+        $(".modal").fadeIn(300);
+        $(".warning").fadeIn(300)
+      })
+
+    this.answer = data["ans"]
+  }
+
+  selectQuestion() {
+    let randomNumber = this.getRandomNumber()
+
+    // Change stage data at every stage length of 6
+    if(this.stageLength < 6) {
+      this.changeQuestionStageData(stages.stage1.data[randomNumber])
+    }
+    else if(this.stageLength < 12) {
+      this.changeQuestionStageData(stages.stage2.data[randomNumber])
+    }
+    else{
+      this.changeQuestionStageData(stages.stage3.data[randomNumber])
+    }
+  }
+
+  start() {
+    $('.welcome').fadeOut(500, function() {
+      $('#game').fadeIn(500)
+    });
+    this.selectQuestion()
+  }
+
+  displaySuccessMessage() {
+    $('.right-wrapper').fadeIn(800);
+    setTimeout(function(){$('#right').html("Correcto!!!")},500)
+    setTimeout(() => {$('#right').html("Felicitaciones!!! gano " + this.amount)},1800)
+    setTimeout(function(){$('#right').html("Siguiente pregunta")},3800)
+    setTimeout(function(){
+      $('.right-wrapper').fadeOut(800, function() {
+        $('#right').html("")
+        $(".modal").fadeOut()
+      })
+    }, 4500)
+  }
+
+  rightAnswer() {
+    if(this.stageLength < 15) {
+      this.stageLength++
+      this.answeredQuestion = (this.stageLength % 6 == 0) || (this.stageLength % 11 == 0) ? [] : this.answeredQuestion;
+      this.amount = $(".stages button").eq(this.pLen-1).html();
+
+      this.displaySuccessMessage()
+
+      this.pLen--;
+      $(".current").removeClass("current");
+      $(".stages button").eq(this.pLen).addClass("current");
+      $(".score").html("Score: " + this.amount)
+      this.gameLength++;
+
+      setTimeout(() => {
+        this.selectQuestion()
+      }, 4500)
+    }
+    else{
+      this.displayWinnerMessage()
+    }
+  }
+
+  displayWinnerMessage() {
+    $('.right-wrapper').fadeIn(800);
+    setTimeout(function(){$('#right').html("Correcto!!!")},500)
+    setTimeout(function(){$('#right').html("Felicitaciones!!! gano")}, 1800)
+    setTimeout(() => {
+      $('.right-wrapper').fadeOut(800, () => {
+        $('#right').html("")
+        this.resetGame()
+      })
+    }, 2300)
+  }
+
+  wrongAnswer() {
+    $('.wrong-wrapper').fadeIn(800);
+
+    setTimeout(() => {
+      $('#wrong').html(`NO!!!<br>Tenia ${this.amount}`)
+    },100)
+
+    setTimeout(
+      function(){
+        $('.wrong-wrapper').fadeOut(function(){
+          $('#wrong').html("")
+          $(".modal").fadeOut(800)
+        })
+      }
+    ,1500)
+
+    setTimeout(() => { this.resetGame() }, 1500) 
+  }
+
+  fiftyFiftyLifeline() {
+    var options = ["D", "A", "C", "B"]
+    var removedOptions = []
+    $(".fifty").attr({"src":"images/fifty2.png"}).css("cursor","default")
+    $(".fifty:hover").css("background-color","rgb(17, 17, 138)")
+    this.enableFiftyFiftyLifeline = false
+
+    for(var i = 0; i < options.length; i++){
+      if(options[i] != this.answer){
+        if(removedOptions.length < 2){
+          removedOptions.push(options[i])
+          for(var i = 0; i < removedOptions.length; i++){
+            $(`div.${removedOptions[i]}`).html(`${removedOptions[i]}:`)
+          }
+        }
+      }
+    }
+  }
+
+  displayGameStatusMessage(message, timeout) {
+    if(!timeout) {
+      $('#chat').html(message)
+      return
+    }
+    setTimeout(() => {
+      $('#chat').html(message)
+    }, timeout)
+  }
+
+
+
+
+
+  callFriendLifeline() {
+    let friendRandomNumber = Math.floor(Math.random()*7);
+    let friendRandomResponse = Math.floor(Math.random()*4)
+    let friends = ["Juan", "Pedro", "Jose", "Carola", "Maria", "Andres", "Antonieta"]
+    let confidencePercentage = ["100%", "80%", "60%", "50%", "30%"]
+    let responseMessage = ["Am sure it's", "It's certainly", "It's definitely", "I think it's", "Am not sure to choose"]
+
+    $(".call").attr({"src":"images/call2.png"}).css("cursor","default")
+    $(".call:hover").css("background-color", "rgb(17, 17, 138)")
+    $(".modal").fadeIn()
+    $('.chat-wrapper').fadeIn(500);
+
+    this.displayGameStatusMessage("Calling... ☎", 100)
+    this.displayGameStatusMessage("Connected ✔", 2000)
+
+    // setTimeout(() => {
+    //   const playerMessage = `ME: Hello ${friends[friendRandomNumber]}. 😞 Am in a hot seat now and i need the answer to this question.<br>${$('.question').html()}`
+    //   this.displayGameStatusMessage(playerMessage)
+    // }, 3800)
+
+    // setTimeout(() => {
+    //   this.displayGameStatusMessage(`${friends[friendRandomNumber]}: Thinking...`)
+    // }, 7000)
+
+    // setTimeout(() => {
+    //   const message = `${friends[friendRandomNumber]}: ${responseMessage[friendRandomResponse]} ${this.answer}.`
+    //   this.displayGameStatusMessage(message)
+    // }, 10000)
+
+    // setTimeout(() => {
+    //   this.displayGameStatusMessage("ME: How sure are you?")
+    // }, 13000)
+
+    // setTimeout(() => {
+    //   const message = `${friends[friendRandomNumber]}: ${confidencePercentage[friendRandomResponse]} sure.`
+    //   this.displayGameStatusMessage(message)
+    // }, 16000)
+
+    setTimeout(() => {
+      $('.chat-wrapper').fadeOut(800, () => {
+        this.displayGameStatusMessage("")
+        $(".modal").fadeOut()
+        this.enableCallFriendLifeline = false
+      })
+    }, 18000)
+  }
+
+
+
+
+
+
+  askAudienceLifeline() {
+    $(".aud").attr({"src":"images/aud2.png"}).css("cursor","default")
+    $(".aud:hover").css("background-color","rgb(17, 17, 138)")
+    $(".modal").fadeIn()
+    $(".chat-wrapper").fadeIn(500)
+            
+    setTimeout(() => {
+      this.displayGameStatusMessage("Throwing question to audience...")
+    }, 100)
+
+    setTimeout(() => {
+      this.displayGameStatusMessage("Audience thinking...")
+    }, 1200)
+
+    setTimeout(() => {
+      $(".chat-wrapper").fadeOut()
+      this.displayGameStatusMessage("")
+      // $('.audience-wrapper').fadeIn()
+      this.enableAskAudienceLifeline = false
+    }, 2700)
+
+    var options = ["D", "A", "C", "B"]
+    var audiencePercentage = ["15", "32", "48", "54", "60"]
+
+    for(var i = 0; i < options.length; i++){
+      if(options[i] != this.answer){
+        $(`.bar-${options[i]}`).css("width", `${audiencePercentage[i]}%`)
+      }else {
+        var highestPercentage = Number(audiencePercentage[4]) + 
+                                  Math.floor(Math.random() * (23 -10)) + 10
+        $(`.bar-${this.answer}`).css("width", `${highestPercentage}%`)
+      }
+    }
+
+    $(".closeBtn").click(function(){
+      $(".audience-wrapper").fadeOut()
+      $(".modal").fadeOut()
+    })
+  }
+
+
+
+
+
+
+  resetGame() {
+    $(".modal").fadeOut()
+    $("#game").fadeOut()
+
+    setTimeout(
+      function(){$(".welcome").fadeIn(600)}
+    ,1000)
+
+    this.enableCallFriendLifeline = true
+    this.enableAskAudienceLifeline = true
+    this.enableFiftyFiftyLifeline = true
+    this.stageLength = 1;
+    this.answeredQuestion = [];
+
+    this.gameLength = $(".stages button").length
+    this.pLen = this.gameLength
+    this.amount = $(".stages button").eq(this.pLen-1).html();
+        
+    $("img:hover").css("background-color","rgb(250, 121, 0) !important")
+    $(".fifty").attr({"src":"images/fifty.png","onClick":"game.fifty()"}).css("cursor","pointer")
+    $(".callFriend").attr({"src":"images/call.png","onClick":"game.call_a_friend()"}).css("cursor","pointer")
+  }
+
+
+}
+
+/**
+ * Mouse event handlers
+ */
+(function initGame() {
+  const game = new Game()
+  $(".no").click(function() {
+    $(".modal").fadeOut()
+  });
+
+  $(".yes").click(function(){
+    $('.warning').fadeOut(500,
+      function(){
+        (game.selected == game.answer) ? game.rightAnswer() : game.wrongAnswer()
+      }
+    )
+  })
+
+  $(".about-btn").click(function(){
+    $('.about').fadeIn(1000);
+  })
+
+  $(".fifty").click(function(){
+    if(game.enableFiftyFiftyLifeline)
+      game.fiftyFiftyLifeline()
+  })
+
+  $(".call").click(function(){
+    if(game.enableCallFriendLifeline)
+      game.callFriendLifeline()
+  })
+
+  $(".aud").click(function(){
+    if(game.enableAskAudienceLifeline)
+      game.askAudienceLifeline()
+  })
+
+  $(".start-btn").click(function(){
+    game.start()
+  })
+})()
